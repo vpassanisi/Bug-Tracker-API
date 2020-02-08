82c40f20084b67f46358dad2f919ae732a63f5e7
vpassanisi <vpassanisi@gmail.com>
2020-02-07 19:36:44 -0800

    tests

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ require("colors");
 const server = require("./server");
 
 const port = process.env.PORT || 5500;
+
 server.listen(port, () =>
   console.log(`Server is running on ${port}`.yellow.bold)
 );
