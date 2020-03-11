96c9531e4e011059f5a4073d1ebb76030d645389
vpassanisi <vpassanisi@gmail.com>
2020-03-11 16:42:09 -0700

    fixed link

--- client/src/Navbar.svelte
+++ client/src/Navbar.svelte
@@ -2,12 +2,13 @@
   import { login } from "./stores/login";
   import LoggedInNav from "./LoggedInNav.svelte";
   import LoggedOutNav from "./LoggedOutNav.svelte";
+  import { link } from "svelte-spa-router";
 </script>
 
 <nav>
   <div class="nav-wrapper cyan darken-4">
     <div class="container">
-      <a href="#!" class="brand-logo left">Aquila</a>
+      <a href="/" use:link class="brand-logo left">Aquila</a>
       <a
         href={null}
         data-target="mobile-demo"
